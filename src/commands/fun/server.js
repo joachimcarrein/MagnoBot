8b310358b66da22b0e1e873657af6285a74a5f7c
Joachim Carrein <joachim.carrein@ifacto.be>
2022-05-03 17:49:44 +0200

    server stats

---
+++ src/commands/fun/server.js
@@ -0,0 +1,32 @@
+const Discord = require("discord.js")
+module.exports = {
+    name: "server",
+    aliases: [],
+    category: "fun",
+    permissions: 2,
+    run: async ({ client, message, args }) => {
+        let serverEmbed = new Discord.MessageEmbed()
+            .setColor("DARK_GOLD")
+
+        switch (args[0]) {
+            case 'members':
+                const serverMembers = message.guild.memberCount;
+                serverEmbed.setTitle(`${message.guild.name} has ${serverMembers} members.`)
+                break;
+            case 'boosts':
+                const serverBoosts = message.guild.premiumSubscriptionCount
+                serverEmbed.setTitle(`${message.guild.name} has ${serverBoosts} boosts of the server.`)
+                break;
+            case 'joined':
+                serverEmbed.setTitle(`You joined ${message.guild.name} at ${message.member.joinedAt}.`)
+                break;
+            default:
+                serverEmbed.setTitle(`Welcome to server ${message.guild.name}`)
+                break;
+        }
+
+        message.channel.send({ embeds: [serverEmbed] })
+    }
+}
+
+
