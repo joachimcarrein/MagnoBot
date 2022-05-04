88aa513195364d3f4924945f324640e5c37fa4fb
Joachim Carrein <joachim.carrein@ifacto.be>
2022-05-04 14:23:59 +0200

    nuke channel

---
+++ src/commands/channel/nuke.js
@@ -0,0 +1,17 @@
+module.exports = {
+    name: "nuke",
+    aliases: [],
+    category: "channel",
+    permissions: 0,
+    run: async ({ client, message, args }) => {
+        // default deletes message itself plus previous
+        let reason = args.join(" ")
+        if (!reason) reason = "no reason given"
+
+        const nukeChannel = message.channel
+        if (!nukeChannel.deletable) return message.reply("Cannot nuke channel.")
+
+        await nukeChannel.clone().catch(error => console.log(error))
+        await nukeChannel.delete(reason).catch(error => console.log(error))
+    }
+}
