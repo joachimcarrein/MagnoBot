32a4a4497d31700b37dfb70fe5a192eb2bbe7f35
Joachim Carrein <joachim.carrein@ifacto.be>
2022-05-04 16:46:41 +0200

    description, allow user forced bot game

--- src/commands/fun/tictactoe.js
+++ src/commands/fun/tictactoe.js
@@ -4,11 +4,15 @@ module.exports = {
     name: "tictactoe",
     aliases: ["ttt"],
     category: "fun",
-    description: "Play tic tac toe. Use parameter p for 2 player game.\nChance of bot start = 1/5.\nIf bot starts, you probably gonna loose.",
+    description: "Play tic tac toe.\n" + 
+                 "Use parameter p for 2 player game. eg. !ttt p\n" + 
+                 "Chance of bot start = 1/5.\n" + 
+                 "Use parameter u for bot game, but certain user start. eg. !ttt u\n" +
+                 "If bot starts, you probably gonna loose.",
     permissions: 2,
     run: async ({ client, message, args }) => {
         let target = "c"
-        if (["p","player","u","user", '2p'].includes(args[0])) target = "u"        
+        if (args[0] == "p") target = "u"        
 
         let game = "........." // 9 dots to represent playing field, . = empty, x = X, o = O
         let currTurn = "o" // easier than loads of checks for right print name
