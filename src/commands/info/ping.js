13f278aae7963afbc711f1c7dcf78c618a7014bf
Joachim Carrein <joachim.carrein@ifacto.be>
2022-05-04 19:12:46 +0200

    format

--- src/commands/info/ping.js
+++ src/commands/info/ping.js
@@ -5,6 +5,9 @@ module.exports = {
     description: 'Simple ping with timings',
     run: async ({ client, message, args }) => {
         const msg = await message.channel.send(`:ping_pong: Main bot Pinging...`);
-        msg.edit(`:ping_pong: Pong! \nAPI: \`${Math.round(client.ws.ping)}\`ms\nBot: \`${msg.createdAt - message.createdAt}\`ms.\nUptime: ${client.functions.get("functions").formatTime(client.uptime)}`);
+        msg.edit(`:ping_pong: Pong! \n` + 
+                 `API: \`${Math.round(client.ws.ping)}\`ms\n` + 
+                 `Bot: \`${msg.createdAt - message.createdAt}\`ms.\n` + 
+                 `Uptime: ${client.functions.get("functions").formatTime(client.uptime)}`);
     }
 }
