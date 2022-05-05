84c01d9545e0b2da9d9c1f7b944f7c064f07e8f7
Joachim Carrein <joachim.carrein@ifacto.be>
2022-05-05 18:06:33 +0200

    clear improve

--- src/commands/channel/clear.js
+++ src/commands/channel/clear.js
@@ -12,7 +12,7 @@ module.exports = {
         //if argument is provided, we need to convert it from string to number
         if (args[0]) {
             //add 1 to delete clear command itself
-            num = parseInt(args[0]) + 1;
+            num = parseInt(args[0]);
         }
 
         if (num > 100) num = 100
@@ -21,7 +21,7 @@ module.exports = {
         message.channel.bulkDelete(num);
 
         //notify channel of deleted messages
-        let newmsg = await message.channel.send(`Deleted ${num - 1} posts.\nThis message will be removed in 5 seconds.`);
+        let newmsg = await message.channel.send(`Deleted ${num} posts.\nThis message will be removed in 5 seconds.`);
 
         await client.functions.get("functions").delay(5000)
         try {
