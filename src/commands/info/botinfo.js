0ef2bfcfac01cdf29ab9915a2763424500162e4b
Joachim Carrein <joachim.carrein@ifacto.be>
2022-05-05 16:46:22 +0200

    botinfo

---
+++ src/commands/info/botinfo.js
@@ -0,0 +1,27 @@
+const pjson = require("../../../package.json");
+const Discord = require("discord.js")
+
+module.exports = {
+    name: "botinfo",
+    aliases: [],
+    category: "info",
+    description: 'Displays bot info',
+    run: async ({ client, message, args }) => {
+        const botMember = await message.guild.members.fetch(client.user.id)
+        const botembed = new Discord.MessageEmbed()
+            .setThumbnail(client.user.displayAvatarURL)
+            .setTitle("About this bot:")
+            .setAuthor({name: client.user.username, iconURL: client.user.displayAvatarURL()})
+            .setDescription("Simple bot by MagnoBE")
+            .setColor("#800080")
+            .addField("Bot name:",client.user.username, true)
+            .addField("Version:", `${pjson.version} ${pjson.codeName}`, true)
+            .addField("Created by:", "MagnoBE", true)
+            .addField("Created on", `${client.user.createdAt}`, true)
+            .addField("On the server since:", `${botMember.joinedAt}`, true)
+            .addField("Guilds Using this bot: ", `\`${client.guilds.cache.size}\``, true)
+            .setTimestamp()
+
+        return await message.reply({ embeds: [botembed] });
+    }
+}
