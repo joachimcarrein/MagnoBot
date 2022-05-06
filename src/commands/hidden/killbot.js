9dff6be06230b8549f2afeaf242c12822e4e6512
Joachim Carrein <joachim.carrein@ifacto.be>
2022-05-06 11:05:58 +0200

    kill bot admin cmd

---
+++ src/commands/hidden/killbot.js
@@ -0,0 +1,11 @@
+module.exports = {
+    name: "killbot",
+    aliases: [],
+    category: "hidden",
+    permissions: -1,
+    description: 'Kill bot process.',
+    usage: "",
+    run: async ({ client, message, args }) => {
+        process.exit()
+    }
+}
