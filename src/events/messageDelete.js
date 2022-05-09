3aa113932e8b0f624c02d4407e03ff131596a6c4
Joachim Carrein <joachim.carrein@ifacto.be>
2022-05-09 16:33:02 +0200

    ignore deleted bot messages

--- src/events/messageDelete.js
+++ src/events/messageDelete.js
@@ -2,6 +2,9 @@ module.exports = {
     name: "messageDelete",
     run: async (bot, message) => {
         const {client} = bot
+        
+        if (message.author.bot) return // ignore deleted bot messages
+
         client.snipes.set(message.channel.id, {
             content: message.content,
             author: message.author
