b7737c8e4bce905264c56b3de220e37c4ca12afa
Joachim Carrein <joachim.carrein@ifacto.be>
2022-05-17 16:50:00 +0200

    edit level

--- src/slashcommands/levels/editlevel.js
+++ src/slashcommands/levels/editlevel.js
@@ -1,75 +1,111 @@
 const Levels = require('discord-xp')
 module.exports = {
     name: "editlevel",
-    aliases: [],
     category: "levels",
     permissions: 20,
     description: 'edit a user level',
-    usage: "<user> <xp | level> <add | set | remove> <number>",
-    run: async ({ client, message, args}) => {
-        let guildSettings = await client.functions.get("functions").getGuildSettings(message.guild.id)
-        let usage = `${guildSettings.prefix}editlevel @member [xp, level] [add, set, remove] <number>`
+    options: [
+        {
+            name: "user",
+            description: "User to change",
+            type: "USER",
+            required: true
+        },
+        {
+            name: "what",
+            description: "What to update",
+            type: "STRING",
+            choices: [ 
+                {
+                    name: "xp", 
+                    value: "xp"
+                }, 
+                {
+                    name: "level", 
+                    value:"level"
+                }
+            ],
+            required: true
+        },
+        {
+            name: "action",
+            description: "What to do",
+            type: "STRING",
+            choices: [ 
+                {
+                    name: "add", 
+                    value: "add"
+                }, 
+                {
+                    name: "set", 
+                    value:"set"
+                },
+                {
+                    name: "remove", 
+                    value:"remove"
+                }
+            ],
+            required: true
+        },
+        {
+            name: "number",
+            description: "amount to change",
+            type: "INTEGER",
+            required: true
+        },
+    ],
+    run: async ({ interaction}) => {
+        const mentionedMember = interaction.options.getUser("user")
+        const whatToUpdate = interaction.options.getString("what")
+        const action = interaction.options.getString("action")
+        const value = interaction.options.getInteger("number")
 
-        if (!args[0]) return message.reply(`Please supply the necessary arguments.\n\`${usage}\``)
-
-        const mentionedMember = message.mentions.users.first() || message.guild.members.cache.find(entry => entry.user.username === args[0])?.user
-        if (!mentionedMember) return message.reply('the member stated is not in the server')
-
-        if (!args[1]) return message.reply(`You need to state if editing level or xp.\n\`${usage}\``)
-        if (!['xp', 'level'].includes(args[1])) return message.reply(`Your second argument was not level or xp.\n\`${usage}\``)
-
-        if (args[1] == 'xp') {
-            if (!['add', 'set', 'remove'].includes(args[2])) return message.reply(`You need to state if adding, setting or removing xp.\n\`${usage}\``)
-            const value = Number(args[3])
-            const levelUser = await Levels.fetch(mentionedMember.id, message.guild.id)
-            if (!levelUser) return message.reply(`Member not registered in the database yet.`)
-            if (value === undefined) return message.reply(`The number is not a valid number.\n\`${usage}\``)
-            if (args[2] == 'add') {
+        if (whatToUpdate == 'xp') {
+            const levelUser = await Levels.fetch(mentionedMember.id, interaction.guild.id)
+            if (!levelUser) return interaction.reply(`Member not registered in the database yet.`)
+            if (action == 'add') {
                 try {
-                    await Levels.appendXp(mentionedMember.id, message.guild.id, value)
-                    message.reply(`Added ${value} xp to ${mentionedMember.username}`)
+                    await Levels.appendXp(mentionedMember.id, interaction.guild.id, value)
+                    interaction.reply(`Added ${value} xp to ${mentionedMember.username}`)
                 } catch (error) {
                     console.log(error)
                 }
-            } else if (args[2] == 'remove') {
+            } else if (action == 'remove') {
                 try {
-                    await Levels.subtractXp(mentionedMember.id, message.guild.id, value)
-                    message.reply(`Removed ${value} xp from ${mentionedMember.username}`)
+                    await Levels.subtractXp(mentionedMember.id, interaction.guild.id, value)
+                    interaction.reply(`Removed ${value} xp from ${mentionedMember.username}`)
                 } catch (error) {
                     console.log(error)
                 }
-            } else if (args[2] == 'set') {                
+            } else if (action == 'set') {                
                 try {
-                    await Levels.setXp(mentionedMember.id, message.guild.id, value)
-                    message.reply(`Set ${value} xp for ${mentionedMember.username}`)
+                    await Levels.setXp(mentionedMember.id, interaction.guild.id, value)
+                    interaction.reply(`Set ${value} xp for ${mentionedMember.username}`)
                 } catch (error) {
                     console.log(error)
                 }
             }
-        } else if (args[1] == 'level') {
-            if (!['add', 'set', 'remove'].includes(args[2])) return message.reply(`You need to state if adding, setting or removing levels.\n\`${usage}\``)
-            const value = Number(args[3])
-            const levelUser = await Levels.fetch(mentionedMember.id, message.guild.id)
-            if (!levelUser) return message.reply(`Member not registered in the database yet.`)
-            if (value === undefined) return message.reply(`The number is not a valid number.\n\`${usage}\``)
-            if (args[2] == 'add') {
+        } else if (whatToUpdate == 'level') {
+            const levelUser = await Levels.fetch(mentionedMember.id, interaction.guild.id)
+            if (!levelUser) return interaction.reply(`Member not registered in the database yet.`)
+            if (action == 'add') {
                 try {
-                    await Levels.appendLevel(mentionedMember.id, message.guild.id, value)
-                    message.reply(`Added ${value} levels to ${mentionedMember.username}`)
+                    await Levels.appendLevel(mentionedMember.id, interaction.guild.id, value)
+                    interaction.reply(`Added ${value} levels to ${mentionedMember.username}`)
                 } catch (error) {
                     console.log(error)
                 }
-            } else if (args[2] == 'remove') {
+            } else if (action == 'remove') {
                 try {
-                    await Levels.subtractLevel(mentionedMember.id, message.guild.id, value)
-                    message.reply(`Removed ${value} levels from ${mentionedMember.username}`)
+                    await Levels.subtractLevel(mentionedMember.id, interaction.guild.id, value)
+                    interaction.reply(`Removed ${value} levels from ${mentionedMember.username}`)
                 } catch (error) {
                     console.log(error)
                 }
-            } else if (args[2] == 'set') {                
+            } else if (action == 'set') {                
                 try {
-                    await Levels.setLevel(mentionedMember.id, message.guild.id, value)
-                    message.reply(`Set ${value} levels for ${mentionedMember.username}`)
+                    await Levels.setLevel(mentionedMember.id, interaction.guild.id, value)
+                    interaction.reply(`Set ${value} levels for ${mentionedMember.username}`)
                 } catch (error) {
                     console.log(error)
                 }
