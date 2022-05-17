767c39a6cb6fd039b7cd650bbab8a0c35d018325
Joachim Carrein <joachim.carrein@ifacto.be>
2022-05-17 09:49:51 +0200

    killbot filters

--- src/commands/hidden/killbot.js
+++ src/commands/hidden/killbot.js
@@ -6,10 +6,22 @@ module.exports = {
     description: 'Kill bot process.',
     usage: "",
     run: async (bot) => {
-        var {  message } = bot;
+        var { message, args } = bot;
+
+        const request = args.join(" ")
         const os = require("os")
-        await message.reply(`Killing bot process on \`${os.hostname()}\`. No recovery from this... 😵🪦`)
-        bot.killBot()
-        await message.reply("Something went wrong, bot still here...")
+        let killBot = true
+
+        if (!!request) {
+            killBot = (request.toLowerCase() === os.hostname().toLowerCase())
+        }
+
+        if (killBot) {
+            await message.reply(`Killing bot process on \`${os.hostname()}\`. No recovery from this... 😵🪦`)
+            bot.killBot()
+            await message.reply("Something went wrong, bot still here...")
+        } else {
+            await message.reply(`bot is not \`${request}\` but \`${os.hostname()}\`, skipping kill.`)
+        }
     }
 }
--- src/commands/hidden/restartbot.js
+++ src/commands/hidden/restartbot.js
@@ -6,9 +6,21 @@ module.exports = {
     description: 'Restart bot',
     usage: "",
     run: async (bot) => {
-        var { message } = bot;
+        var { message, args } = bot;
+
+        const request = args.join(" ")
         const os = require("os")
-        await message.reply(`Restarting bot on \`${os.hostname()}\``)
-        bot.restart()
+        let restartBot = true
+
+        if (!!request) {
+            restartBot = (request.toLowerCase() === os.hostname().toLowerCase())
+        }
+
+        if (restartBot) {
+            await message.reply(`Restarting bot on \`${os.hostname()}\``)
+            bot.restart()
+        } else {
+            await message.reply(`bot is not \`${request}\` but \`${os.hostname()}\`, skipping restart.`)
+        }
     }
 }
